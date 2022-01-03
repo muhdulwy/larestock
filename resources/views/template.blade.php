5975a5b2edbb3262f65a2228a162e2a63bd6320d
ulwy <ulwy.rpl@gmail.com>
2022-01-03 22:16:56 +0700

    dashboard & navbar

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -1,17 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+  
+    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <title>WEB PROGRAMMING</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>LaRestock</title>
 
     @yield('css')
 </head>
 <body>
-    
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary shadow-sm ">
+        <div class="container">
+            <a class="navbar-brand" href="{{route('home')}}"> <h2>LaRestock</h2> </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button> 
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav ms-auto">
+                <a class="nav-link active" aria-current="page" href="{{route('home')}}">Home</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Barang
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="{{route('barang.index')}}">Barang</a></li>
+                      <li><a class="dropdown-item" href="{{route('barang.create')}}">Input Barang</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Stock
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <li><a class="dropdown-item" href="{{route('stock.index')}}">Stock</a></li>
+                      <li><a class="dropdown-item" href="{{route('stock.create')}}">Input Stock</a></li>
+                    </ul>
+                </li>
+                
+                {{-- <a class="nav-link" href="#Log out">Log out</a> --}}
+                <a class="nav-link" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
+            </div>
+        </div>
+        </nav>
     <div class="container">
         @yield('content')
     </div>
